ae5d86efde5c098860cbdf0cb2843634bf32602d
Andrew Furth <andrew.furth@gmail.com>
2016-07-10 15:32:30 -0700

    questions.js route file created

---
+++ server/routes/questions.js
@@ -0,0 +1,23 @@
+var express = require('express')
+    router = express.Router(),
+    db = require('../models'),
+    Promise = require("bluebird");
+
+router.route('/')
+  .get((req, res) => {
+    
+  })
+  .post((req, res) => {
+    
+  })
+
+router.route('/:id')
+  .get((req, res) => {
+    
+  })
+  .put((req, res) => {
+    
+  })
+  .delete((req, res) => {
+    
+  })
