1f01a37c6d755f385bab46efcb3bdf874e231efe
Andrew Furth <andrew.furth@gmail.com>
2016-07-17 19:10:28 -0700

    session start page formatted

--- client/javascripts/controllers/sessionController.js
+++ client/javascripts/controllers/sessionController.js
@@ -95,6 +95,30 @@
       console.log("Session data: ", vm.session)
       console.log("User data: ", vm.trackingData)
 
+      vm.setCatClass = function(location, category) {
+        // Set class for Category label
+        if (category === "Math") {
+          location.catClass = "label-danger"
+          location.catIcon = "fa-calculator"
+        } else if (category === "English") {
+          location.catClass = "label-info"
+          location.catIcon = "fa-book"
+        } else if (category === "Social Studies") {
+          location.catClass = "label-default"
+          location.catIcon = "fa-globe"
+        } else {
+          location.catClass = "label-warning"
+          location.catIcon = "fa-flask"
+        }
+      }
+
+      // Set CatClass for Test
+      vm.setCatClass(vm.session._test, vm.session._test.category)
+      // Set CatClass for Questions in Test
+      vm.session.answers.forEach((val) => {
+        vm.setCatClass(val._question, val._question.category)
+      })
+
       vm.testLength = vm.session._test.questions.length;
       vm.uChoice = null;
       vm.result = {
