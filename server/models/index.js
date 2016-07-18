b9b709f483edf9c1e3c37d9746ac1d16fca9fb3b
Andrew Furth <andrew.furth@gmail.com>
2016-07-18 16:07:44 -0700

    changes to environment vars when connecting to mongo db

--- server/models/index.js
+++ server/models/index.js
@@ -2,8 +2,12 @@ require('dotenv').load();
 
 var Promise = require("bluebird");
 var mongoose = Promise.promisifyAll(require("mongoose"));
-var MONGOLAB_URI = 'mongodb://'+process.env.DB_USERNAME+':'+process.env.DB_PASSWORD+'@ds023455.mlab.com:23455/osqar_db'
-mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost/OSQAR")
+
+if (process.env.DB_USERNAME && process.env.DB_PASSWORD) {
+  mongoose.connect('mongodb://'+process.env.DB_USERNAME+':'+process.env.DB_PASSWORD+'@ds023455.mlab.com:23455/osqar_db')
+} else {
+  mongoose.connect("mongodb://localhost/OSQAR")
+}
 mongoose.set("debug", true);
 
 // module.exports.MODEL_NAME = require("./model_name")
